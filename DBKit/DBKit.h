8e7ea9432f6c920ca446a49d860a3cb5faeaede3
Etresoft <info@etresoft.com>
2014-04-10 13:06:06 -0700

    Add top-level include file.

---
+++ DBKit/DBKit.h
@@ -0,0 +1,11 @@
+/***********************************************************************
+ ** Etresoft
+ ** John Daniel
+ ** Copyright (c) 2014. All rights reserved.
+ **********************************************************************/
+
+#import "DB.h"
+#import "DBSelect.h"
+#import "DBInsert.h"
+#import "DBUpdate.h"
+#import "DBDelete.h"
